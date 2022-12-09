09659dd1c048c67aadbed51f6df9a7226a3c43be
Phoenix <prodotzip@gmail.com>
2022-12-10 02:52:08 +0530

    setup LinearQNet

--- agent.py
+++ agent.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 
 from game import SnakeGameAI, Direction, Point
+from model import LinearQNet, QTrainer
 
 MAX_MEMORY = 100000
 BATCH_SIZE = 1000
@@ -15,10 +16,10 @@ class Agent:
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0  # randomness
-        self.gamma = 0  # discount rate
+        self.gamma = 0.9  # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)
-        self.model = None
-        self.trainer = None
+        self.model = LinearQNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
     def get_state(self, game):
         head = game.snake[0]
@@ -68,9 +69,10 @@ class Agent:
             mini_sample = self.memory
 
         states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
         self.epsilon = 80 - self.n_games
@@ -80,7 +82,7 @@ class Agent:
             final_move[random.randint(0, 2)] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model.predict(state0)
+            prediction = self.model(state0)
             final_move[torch.argmax(prediction).item()] = 1
 
         return final_move
